2d3bb038e9a892ef43839340b90e2e5d56c29d60
Morris Hsu <morris754@gmail.com>
2024-11-13 08:24:30 +0000

    add itgc

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("📄 Document question answering")
+st.title("📄 ITGC Change Access Check")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
+    "Upload a Jira ticket and we will help check if change access controls! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
